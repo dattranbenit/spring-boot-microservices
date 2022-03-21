3a96eb88baa04359786d0fae7f029ab477073a32
Your Name <dat@Dats-MacBook-Pro.local>
2022-03-21 15:45:19 +0700

    tracing log statistic service

--- src/main/java/statisticservice/controller/StatisticController.java
+++ src/main/java/statisticservice/controller/StatisticController.java
@@ -23,12 +23,12 @@ public class StatisticController {
     @PostMapping("/statistic")
     public StatisticDTO add(@RequestBody StatisticDTO statisticDTO) {
         logger.info("Add statistic");
-    //	try {
-    //	    Thread.sleep(10000);
-    //	} catch (InterruptedException e) {
-    //	    // TODO Auto-generated catch block
-    //	    e.printStackTrace();
-    //	}
+//    	try {
+//    	    Thread.sleep(10000);
+//    	} catch (InterruptedException e) {
+//    	    // TODO Auto-generated catch block
+//    	    e.printStackTrace();
+//    	}
         statisticService.add(statisticDTO);
         return statisticDTO;
     }
