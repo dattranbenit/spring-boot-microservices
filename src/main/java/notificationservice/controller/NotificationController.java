8c1d17b256684d9e9eec36fc4f8662108ca9b2db
dattranbenit <dattran.benitl@gmail.com>
2022-06-05 01:20:54 +0700

    commit

--- src/main/java/notificationservice/controller/NotificationController.java
+++ src/main/java/notificationservice/controller/NotificationController.java
@@ -3,6 +3,7 @@ package notificationservice.controller;
 import notificationservice.dto.MessageDTO;
 import notificationservice.service.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class NotificationController {
     @Autowired
     private EmailService emailService;
 
+    @PreAuthorize("#oauth2.hasScope('notification')")
     @PostMapping("/send-notification")
     public void sendNotification(@RequestBody MessageDTO messageDTO) {
 	    emailService.sendEmail(messageDTO);
