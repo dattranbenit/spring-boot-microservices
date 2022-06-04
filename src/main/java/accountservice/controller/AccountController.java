6439c09368be665354aa3cfd005200333c98d8b1
dattranbenit <dattran.benitl@gmail.com>
2022-06-04 19:56:19 +0700

    commit

--- src/main/java/accountservice/controller/AccountController.java
+++ src/main/java/accountservice/controller/AccountController.java
@@ -9,8 +9,13 @@ import accountservice.service.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.PermitAll;
+import java.security.Principal;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -43,12 +48,15 @@ public class AccountController {
         return accountDTO;
     }
 
+    @PreAuthorize("#oauth2.hasScope('read') && hasRole('PREMIUM_MEMBER')")
     @GetMapping("/accounts")
     public List<AccountDTO> getAll() {
         statisticService.add(new StatisticDTO("Get all accounts.", new Date()));
         return accountService.getAll();
     }
 
+    @PreAuthorize("#oauth2.hasScope('read') && hasRole('MEMBER')")
+    @PostAuthorize("returnObject.body.username == authentication.name || hasRole('PREMIUM_MEMBER')")
     @GetMapping("/account/{id}")
     public ResponseEntity<AccountDTO> get(@PathVariable(name = "id") Long id) {
         statisticService.add(new StatisticDTO("Account " + id + " is trying to be accessed.", new Date()));
@@ -56,15 +64,23 @@ public class AccountController {
                 .orElse(new ResponseEntity<AccountDTO>(HttpStatus.NOT_FOUND)); // fail tra ve status not found
     }
 
+    @Secured("ROLE_PREMIUM_MEMBER")
     @DeleteMapping("/account/{id}")
     public void delete(@PathVariable(name = "id") Long id) {
         statisticService.add(new StatisticDTO("Account " + id + " is deleted.", new Date()));
         accountService.delete(id);
     }
 
+    @PreAuthorize("#oauth2.hasScope('write') && hasRole('PREMIUM_MEMBER')")
     @PutMapping("/account")
     public void update(@RequestBody AccountDTO accountDTO) {
         statisticService.add(new StatisticDTO("Account " + accountDTO.getId() + " is updated.", new Date()));
         accountService.update(accountDTO);
+    }
+
+    @PreAuthorize("#oauth2.hasScope('read') && isAuthenticated()")
+    @GetMapping("/me")
+    public Principal me(Principal principal) {
+        return principal;
     }
 }
